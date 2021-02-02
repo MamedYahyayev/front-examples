139589904dd38d37900f92938ddda7d580e88ce1
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-02 15:44:56 +0400

    Dynamic Imports

--- js_modules/app.js
+++ js_modules/app.js
@@ -1,4 +1,4 @@
-import { Student } from "./Student.js";
+// import { Student } from "./Student.js";
 import { StudentList } from "./StudentList.js";
 
 // dom elements
@@ -9,11 +9,21 @@ const studentList = new StudentList();
 
 addStudentBtn.addEventListener("click", (event) => {
   event.preventDefault();
-
-  const inputName = form.querySelector("#name");
-  const inputSurname = form.querySelector("#surname");
-
-  const student = new Student(inputName.value, inputSurname.value);
-  studentList.addStudent(student);
-  studentList.attachStudent();
+  addStudent();
 });
+
+function addStudent() {
+  import("./Student.js").then((module) => {
+    const inputName = form.querySelector("#name");
+    const inputSurname = form.querySelector("#surname");
+
+    const student = new module.Student(inputName.value, inputSurname.value);
+    studentList.addStudent(student);
+    studentList.attachStudent();
+  });
+}
+
+/**
+ *  Dynamic Imports when we click the add button on the html page and then Student.js file will load immediately.
+ *  This is very helpful for increase performance.
+ */
