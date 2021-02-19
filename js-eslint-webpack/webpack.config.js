434dfb708c3c2f1abaf18a674bfd08d68bab8ea3
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-19 17:25:31 +0400

    webpack-dev-server up and running correctly

--- js-eslint-webpack/webpack.config.js
+++ js-eslint-webpack/webpack.config.js
@@ -2,10 +2,14 @@ const path = require("path");
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  entry: "./src/app.js",
   mode: "development",
+  entry: "./src/app.js",
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "assets", "scripts"),
+    publicPath: "assets/scripts/",
+  },
+  devServer: {
+    port: 9000,
   },
 };
