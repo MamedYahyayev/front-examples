efd1197cd700dc4dc9e28eea505447b6a63f0afc
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-22 11:08:45 +0400

    documentation added

--- js-eslint-webpack/webpack.config.js
+++ js-eslint-webpack/webpack.config.js
@@ -14,3 +14,15 @@ module.exports = {
     port: 9000,
   },
 };
+
+/**
+ *  mode -- this is the mode of project (profile) 2 modes are available : devlopment, production
+ *  entry -- this is the entry point of the project
+ *  output -- webpack generates file according to entry file
+ *    filename -- this is the output file's filename
+ *    path -- where to store this output file
+ *    publicPath -- this is the publicPath
+ *
+ *  devtool -- this is the devtool this helps to debugging code on the browser, by default our source files content don't show
+ *  devServer -- this is the webpack-dev-server configuration default port is 8080.
+ */
