0d12ac2c7e66e8148def95a6ad8c066310c19387
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-23 11:44:51 +0400

    clean plugin and documentation added to production webpack file

--- js-eslint-webpack/webpack.config.prod.js
+++ js-eslint-webpack/webpack.config.prod.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 // eslint-disable-next-line no-undef
 module.exports = {
@@ -13,6 +14,7 @@ module.exports = {
   devServer: {
     port: 9000,
   },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
 
 /**
@@ -25,4 +27,9 @@ module.exports = {
  *
  *  devtool -- this is the devtool this helps to debugging code on the browser, by default our source files content don't show
  *  devServer -- this is the webpack-dev-server configuration default port is 8080.
+ *
+ * plugins -- this is the plugins section, in this section we can insert multiple plugins to use in our project
+ *  for example: CleanPlugin helps us to remove unnecessary files after we build the project, because when we build
+ *  the project old js files don't remove. webpack generate new js files and the old js files don't remove thus we add
+ *  CleanPlugin to remove old and unused js files.
  */
