83507c9be4cebe7b300d7b539fa4ad6d51915a01
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-03-09 17:31:06 +0400

    e2e tested

--- javascript-testing/util.test.js
+++ javascript-testing/util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
@@ -11,3 +12,23 @@ test("should generate valid test output", () => {
 
   expect(text).toBe("Samir (22 years old)");
 });
+
+test("should click around", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ["--window-size=1920,1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto("file:///D:/FrontExamples/javascript-testing/index.html");
+  await page.click("input#name");
+  await page.type("input#name", "Samir");
+
+  await page.click("input#age");
+  await page.type("input#age", "22");
+
+  await page.click("#btnAddUser");
+
+  const finalText = await page.$eval(".user-item", (el) => el.textContent);
+  expect(finalText).toBe("Samir (22 years old)");
+}, 10000);
